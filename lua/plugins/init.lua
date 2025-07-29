07713bd18937c0611041f5df1bca18b26f6aa9c8
Chao Ren <chowypatowy0428@gmail.com>
2025-07-30 01:00:27 +0800

    installed obsidian nvim plugin and set conceal level to 1 in init.lua

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -68,5 +68,25 @@ return {
   },
   {
     "github/copilot.vim",
+    lazy = false,
+  },
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*", -- recommended, use latest release instead of latest commit
+    lazy = true,
+    ft = "markdown",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    opts = {
+      workspaces = {
+        {
+          name = "personal",
+          path = "~/Documents/obsidian vault/",
+        },
+      },
+
+      -- see below for full list of options 👇
+    },
   },
 }
